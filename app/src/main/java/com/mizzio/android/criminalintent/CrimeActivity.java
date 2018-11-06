f5bb0ea923edc3ac550ea6048d81bb19dda88b43
Mionekos <maxrai020797>
2018-11-06 22:24:23 +0500

    fix trouble in gradle app: program type already present

--- app/src/main/java/com/mizzio/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/mizzio/android/criminalintent/CrimeActivity.java
@@ -1,7 +1,8 @@
 package com.mizzio.android.criminalintent;
 
-import android.support.v4.app.Fragment;
+
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
